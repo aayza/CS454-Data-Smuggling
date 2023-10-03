f1394f4001c372264ad505e519067ee9cfee4c20
Evelyn <evelyn.cheng.2020@uni.strath.ac.uk>
2023-10-03 08:55:25 +0100

    RGB values extraction triplets

--- image_operations.py
+++ image_operations.py
@@ -33,6 +33,27 @@ def extract_bmp_channels_ints(filename = default_filename):
     except Exception as e:
         return str(e)
 
+def extract_bmp_pixels_as_rgb(filename = default_filename):
+    output = []
+
+    try:
+        with Image.open(filename) as img:
+            header = img.info
+            width, height = img.size
+            # iterating through from top row and across
+            for y in range(height):
+                current_row = []
+
+                for x in range(width):
+                    (r, g, b) = img.getpixel((x, y))
+                    current_row.append((r,g, b))
+
+                output.append(current_row)
+
+            return header, img.size, output
+    except Exception as e:
+        return str(e)
+
 # Helper method for encoding, image must be a reference to an image copy
 # new_colour is a triple tuple, e.g. (255, 0, 0)
 def set_pixel_colour(image, x, y, new_colour):
@@ -57,6 +78,12 @@ if __name__ == "__main__":
     print("Red", red_as_binary)
     print("Green", green_as_binary)
     print("Blue", blue_as_binary)
+
+    # As RGB per pixel
+    header_info, img_size, output = extract_bmp_pixels_as_rgb()
+    print("Header", header_info)
+    print("Image size", img_size)
+    print("RGB pixels", output)
 
     # set new colour of pixel 0, 0 of the test image
     with Image.open(default_filename) as img:
