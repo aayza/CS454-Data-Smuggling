c2f3385ffb2a72af2a15797b64f9348e9a5af0f9
Evelyn <evelyn.cheng.2020@uni.strath.ac.uk>
2023-10-03 08:40:00 +0100

    ints and bins

--- image_operations.py
+++ image_operations.py
@@ -1,6 +1,9 @@
 from PIL import Image
 
-def extract_bmp_channels_into_binary(filename = "input_images/test.bmp"):
+
+default_filename = "input_images/test.bmp"
+
+def extract_bmp_channels_ints(filename = default_filename):
     red_channel = []
     green_channel = []
     blue_channel = []
@@ -18,14 +21,9 @@ def extract_bmp_channels_into_binary(filename = "input_images/test.bmp"):
                 for x in range(width):
                     r, g, b = img.getpixel((x, y))
 
-                    # 8-bit binary representation
-                    r_binary = format(r, '08b')
-                    g_binary = format(g, '08b')
-                    b_binary = format(b, '08b')
-
-                    red_row.append(r_binary)
-                    green_row.append(g_binary)
-                    blue_row.append(b_binary)
+                    red_row.append(r)
+                    green_row.append(g)
+                    blue_row.append(b)
 
                 red_channel.append(red_row)
                 green_channel.append(green_row)
@@ -35,11 +33,20 @@ def extract_bmp_channels_into_binary(filename = "input_images/test.bmp"):
     except Exception as e:
         return str(e)
 
+
 if __name__ == "__main__":
-    header_info, img_size, red_channel, green_channel, blue_channel = extract_bmp_channels_into_binary()
+    header_info, img_size, red_channel, green_channel, blue_channel = extract_bmp_channels_ints()
 
     print("Header", header_info)
     print("Image size", img_size)
     print("Red channel  ", red_channel)
     print("Green channel", green_channel)
     print("Blue channel ", blue_channel)
+
+    # As binary strings
+    red_as_binary = [[bin(num)[2:] for num in sublistrow] for sublistrow in red_channel]
+    green_as_binary = [[bin(num)[2:] for num in sublistrow] for sublistrow in green_channel]
+    blue_as_binary = [[bin(num)[2:] for num in sublistrow] for sublistrow in green_channel]
+    print("Red", red_as_binary)
+    print("Green", green_as_binary)
+    print("Blue", blue_as_binary)
