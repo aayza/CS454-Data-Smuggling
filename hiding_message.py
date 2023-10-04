a073d6bec8ff56cc47d1f83c29e2aecf78507a9e
Evelyn <evelyn.cheng.2020@uni.strath.ac.uk>
2023-10-04 09:54:35 +0100

    comments

--- hiding_message.py
+++ hiding_message.py
@@ -6,7 +6,7 @@ import time
 
 def hide_message(message_list_of_bin=None, filename=default_filename, n_bits_to_modify=1):
     if message_list_of_bin is None:
-        message_list_of_bin = ["01001101", "01000101", "01001111", "01010111"]
+        message_list_of_bin = ["01001101", "01000101", "01001111", "01010111"] # default message
 
     str_as_bin = "".join(message_list_of_bin)
     bits_length = len(str_as_bin)
@@ -18,12 +18,16 @@ def hide_message(message_list_of_bin=None, filename=default_filename, n_bits_to_
     height = img_size[1]
     print(len(flat_pixel_values), flat_pixel_values)
 
-    # validate lengths will fit
+    # validate number of bits to modify and lengths will fit
+    if n_bits_to_modify > 4:
+        print("Maximum of 4 least significant bits is supported to avoid too much image distortion.")
+        return
+
     if bits_length > n_bits_to_modify * len(flat_pixel_values):
         print(f"You have {bits_length} to encode but only {n_bits_to_modify * len(flat_pixel_values)} available.")
         return
 
-    # Modify your flat pack here
+    # Modify your flat pack of pixel values here (TODO: turn into loop)
     for i in range(len(flat_pixel_values)):
         if n_bits_to_modify >= 1:
             if str_as_bin[current_bit_index] == '1':
@@ -67,7 +71,7 @@ def hide_message(message_list_of_bin=None, filename=default_filename, n_bits_to_
 
     print(flat_pixel_values)
 
-    # save your flat pack to an image
+    # save your flat pack of image values to an image
     new_image = Image.new("RGB", img_size)
     for y in range(height):
         for x in range(width):
@@ -75,7 +79,7 @@ def hide_message(message_list_of_bin=None, filename=default_filename, n_bits_to_
             rgb_values = flat_pixel_values[index:index + 3]  # Extract the RGB triplet
             new_image.putpixel((x, y), tuple(rgb_values))  # Convert the list to a tuple
 
-    # Show or save the image
+    # Show and save the image
     new_image.show()
     output_filename = "output_images/" + str(int(time.time())) + "_output_test.bmp"
     new_image.save(output_filename)
@@ -83,10 +87,12 @@ def hide_message(message_list_of_bin=None, filename=default_filename, n_bits_to_
 
 
 if __name__ == "__main__":
+    # Create a image using defaults
     new_file = hide_message()
     print(new_file)
     print(extract_bmp_pixels_as_rgb_bin(new_file))
 
+    # Specify a custom message
     message = "CAT"
     message_in_bin = string_to_binary(message)
 
