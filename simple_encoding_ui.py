1b560f11332095a9c7b40a59010084f889ea6552
Evelyn <evelyn.cheng.2020@uni.strath.ac.uk>
2023-10-09 11:54:33 +0100

    comments

--- simple_encoding_ui.py
+++ simple_encoding_ui.py
@@ -5,9 +5,11 @@ from string_operations import string_to_binary
 folder_path = "input_images"
 
 def encode_message():
+    # Step 1 Enter a string message (don't go nuts, keep it simple)
     user_input = input("Type in a string message.")
     print(f"Your message is {user_input}")
 
+    # Step 2 Select the number of bits to modify per byte
     while True:
         try:
             n_bits_to_modify = int(input("Please type in a number between 1 and 4 inclusive for the number of bits you wish to modify per byte: "))
@@ -20,6 +22,7 @@ def encode_message():
 
     print(f"You typed the number: {n_bits_to_modify}")
 
+    # Step 3 Select an image to hide your message in
     bmp_image_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".bmp")]
 
     if not bmp_image_files:
@@ -38,6 +41,7 @@ def encode_message():
                 selected_image = bmp_image_files[choice - 1]
                 print(f"You selected: {selected_image}")
 
+                # Step 4 Hide the message
                 string_binary = string_to_binary(user_input)
                 hide_message(string_binary, selected_image, n_bits_to_modify)
                 break
